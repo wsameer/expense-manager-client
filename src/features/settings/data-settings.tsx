048c14d59f7173c2ba07eaff4dce9c9dc7fdefce
wsameer <dev.wsameer@gmail.com>
2025-01-08 18:37:27 -0500

    adds settings pages

---
+++ src/features/settings/data-settings.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Eraser, FileDown, Trash2 } from 'lucide-react';
+
+import { toast } from '@/hooks';
+import { useConfirmDialog } from '@/components/ui/confirmable';
+
+import { ImportDataDialog } from '../import-data';
+import { useDeleteTransaction } from '../transactions/api/delete-transaction';
+import { ListGroup } from '@/components/list-group';
+import { ListItem } from '@/components/list-group/list-item';
+
+export const DataSettings = () => {
+  const { t } = useTranslation(['common', 'settings']);
+  const { openConfirmDialog } = useConfirmDialog();
+  const { deleteAllTransactions } = useDeleteTransaction(
+    new Date().toISOString().slice(0, 7),
+  );
+
+  const handleDeleteAllTransaction = () => {
+    openConfirmDialog({
+      title: t('common:alert.are-you-sure'),
+      message: t('transaction:delete-warning-message'),
+      onConfirm: async () => {
+        try {
+          await deleteAllTransactions();
+          toast({
+            title: t('common:alert.deleted'),
+            description: t('transaction:all-transactions-deleted'),
+          });
+        } catch (error) {
+          console.error('Error deleting account:', error);
+          toast({
+            title: t('common:errors.operation-failed'),
+            description: t('transaction:failed-to-delete'),
+          });
+        }
+      },
+    });
+  };
+
+  return (
+    <div
+      id="data-settings"
+      className="grid grid-cols-1 gap-6"
+    >
+      <ListGroup title={t('settings:export-import')}>
+        <ImportDataDialog />
+        <ListItem
+          icon={
+            <FileDown className="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
+          }
+          label={t('settings:export-to-csv')}
+          rightElement={undefined}
+        />
+        <ListItem
+          icon={
+            <FileDown className="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
+          }
+          label={t('settings:export-to-pdf')}
+          rightElement={undefined}
+        />
+      </ListGroup>
+      <ListGroup title={t('settings:other')}>
+        <ListItem
+          icon={<Trash2 className="h-4 w-4 text-red-600 dark:text-zinc-300" />}
+          label={t('settings:delete-all-transactions')}
+          onClick={handleDeleteAllTransaction}
+          rightElement={undefined}
+        />
+        <ListItem
+          icon={<Eraser className="h-4 w-4 text-red-600 dark:text-zinc-300" />}
+          label={t('settings:erase-data')}
+          rightElement={undefined}
+          variant="danger"
+        />
+      </ListGroup>
+    </div>
+  );
+};
