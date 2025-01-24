0f40eec724181f5e6a4afd2ed60d209bde736641
wsameer <dev.wsameer@gmail.com>
2025-01-23 23:03:25 -0500

    floating bottom nav bar uses button

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -19,6 +19,7 @@ import {
 } from '@/router/routes';
 import { cn } from '@/lib/utils';
 import { AddTransaction } from '@/features/add-transaction';
+import { Button } from '../ui/button';
 
 export const Navigation = () => {
   const { isMobile } = useResponsive();
@@ -41,45 +42,45 @@ export const Navigation = () => {
 
   if (isMobile) {
     return (
-      <nav className="fixed z-50 w-11/12 h-16 max-w-lg -translate-x-1/2 bg-zinc-900 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-900 rounded-full bottom-8 left-1/2">
+      <nav className="fixed z-50 w-11/12 h-16 max-w-lg -translate-x-1/2 bg-zinc-900 dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-900 rounded-full drop-shadow-lg bottom-8 left-1/2">
         <div className="grid h-full max-w-lg grid-cols-5 place-items-center place-content-center p-2 gap-2">
           {navItems.slice(0, -1).map((item, index) => (
             <React.Fragment key={item.path}>
-              <button
-                className={cn(
-                  'inline-flex flex-col items-center justify-center rounded-full w-12 h-12 px-3 hover:text-white hover:bg-zinc-700 dark:hover:bg-zinc-900',
+              <Button
+                className={cn('rounded-full h-12 w-12 hover:text-white hover:bg-zinc-700 dark:hover:bg-zinc-900', 
                   {
                     'text-white bg-zinc-700 dark:bg-zinc-900':
                       location.pathname === item.path,
-                    'text-zinc-500 ': location.pathname !== item.path,
-                  },
+                    'text-zinc-500': location.pathname !== item.path,
+                  }
                 )}
+                variant="link"
                 onClick={() => navigate(item.path)}
-              >
+               >
                 <div className="flex flex-col items-center justify-center">
                   <item.icon className="h-5 w-5" />
                 </div>
-              </button>
+              </Button>
               {index === 1 && <AddTransaction />}
             </React.Fragment>
           ))}
 
-          <button
-            className={cn(
-              'inline-flex flex-col items-center justify-center rounded-full w-12 h-12 px-3 text-zinc-500 hover:text-white hover:bg-zinc-700 dark:hover:bg-zinc-900',
+          <Button
+            className={cn('rounded-full h-12 w-12 text-zinc-500 hover:text-white hover:bg-zinc-700 dark:hover:bg-zinc-900', 
               {
                 'text-white bg-zinc-700 dark:bg-zinc-900':
                   location.pathname.includes(
                     navItems[navItems.length - 1].path,
                   ),
-              },
+              }
             )}
+            variant="link"
             onClick={() => navigate(navItems[navItems.length - 1].path)}
           >
             <div className="flex flex-col items-center justify-center">
               <EllipsisIcon className="h-5 w-5" />
             </div>
-          </button>
+          </Button>
         </div>
       </nav>
     );
