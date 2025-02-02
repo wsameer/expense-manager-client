1ea689cbcbab3b563d6bdd0bf65210de4ce3bb31
wsameer <dev.wsameer@gmail.com>
2025-02-02 09:54:42 -0400

    removes bearer token integration

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,9 @@
-export const TOKEN_KEY = 'sameer.expense-manager.auth.token';
 export const USER_KEY = 'sameer.expense-manager.user';
 export const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`;
 
 const AUTH = API_BASE_URL + 'auth/';
 
-export const GET_CSRF_TOKEN_API = API_BASE_URL + 'sanctum/csrf-cookie';
+// export const GET_CSRF_TOKEN_API = API_BASE_URL + 'sanctum/csrf-cookie';
 export const REGISTRATION_API = AUTH + 'register';
 export const LOGIN_API = AUTH + 'login';
 export const LOGOUT_API = AUTH + 'logout';
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { PageLayout } from '@/components/layout';
-import { useResponsive } from '@/hooks';
+import { useAuth, useResponsive } from '@/hooks';
 import { cn } from '@/lib/utils';
 import { MonthNavigator } from '@/components/shared/month-navigator';
 import { DashboardPage } from '@/features/dashboard';
@@ -9,13 +9,16 @@ import { DashboardPage } from '@/features/dashboard';
 export const DashboardRoute = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
   const { isDesktop } = useResponsive();
+  const user = useAuth();
+  console.log("🚀 ~ DashboardRoute ~ user:", user)
+
 
   const handleMonthChange = (year: number, month: number) => {
     setCurrentDate(new Date(year, month));
   };
 
   return (
-    <PageLayout title="Dashboard">
+    <PageLayout title="">
       <div
         className={cn('grid grid-cols-1 gap-3', {
           'w-1/3': isDesktop,
