9c1a60a69fa802ed9dd719e93953f330e9dfcc38
wsameer <dev.wsameer@gmail.com>
2025-03-05 23:45:47 -0500

    installs zustand and inner for state management

--- src/types/api.ts
+++ src/types/api.ts
@@ -6,23 +6,3 @@ export type BaseEntity = {
 export type Entity<T> = {
   [K in keyof T]: T[K];
 } & BaseEntity;
-
-export enum AccountGroup {
-  CASH = 'CASH',
-  CHEQUING = 'CHEQUING',
-  CREDIT_CARD = 'CREDIT_CARD',
-  SAVINGS = 'SAVINGS',
-  INVESTMENTS = 'INVESTMENTS',
-}
-
-export type Account = Entity<{
-  name: string;
-  group: AccountGroup;
-  balance: number;
-  description: string;
-  paymentAccountId: number | null;
-  id: number;
-  user_id: number;
-  updated_at: string; // date time
-  created_at: string; // date time
-}>;
